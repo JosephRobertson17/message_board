a11bd1753b018c567f40c4a1a498e048de7e164e
JosephRobertson17 <josephrobertson17.jr@gmail.com>
2021-11-03 16:29:13 -0400

    Removing unnecessary button class and fixing issue with logging out while in a chatroom

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,9 +13,10 @@ class SessionsController < ApplicationController
             render :new
         end
     end
-    
+
     def destroy
         session[:user_id] = nil
+        session[:message_board_id] = nil
         redirect_to root_path, notice: "Logged out"
     end
 end
